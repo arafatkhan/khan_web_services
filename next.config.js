29ef0d03367e79f8eb1dfd2d4175fa54ee42626c
arafatkhan <arafatkhanju@gmail.com>
2025-03-28 04:58:56 +0600

    Initial commit with PWA-enabled project

--- next.config.js
+++ next.config.js
@@ -4,7 +4,4 @@ module.exports = {
         GMAIL_USER: 'arafatkhanju@gmail.com',
         GMAIL_PASS: 'fczm xpma mxbt hgmk',
     },
-    devIndicators: {
-        buildActivity: false, // Disable the (N) logo and build activity indicator
-    },
 };
